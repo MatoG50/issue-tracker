0103e9f097c7a121082e7551e3c800c3a8d4b16e
MatoG50 <matog50@gmail.com>
2024-05-22 21:30:21 +0300

    Remove duplicate skeleons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,30 +1,30 @@
-'use client';
-import classNames from 'classnames';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
-import { BsBugFill } from 'react-icons/bs';
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { BsBugFill } from "react-icons/bs";
 
 const NavBar = () => {
   const currentPath = usePathname();
 
   const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
   ];
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href='/'>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
         <BsBugFill />
       </Link>
-      <ul className='flex space-x-6'>
+      <ul className="flex space-x-6">
         {links.map(link => (
           <li
             className={classNames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
             })}
             key={link.href}
           >
